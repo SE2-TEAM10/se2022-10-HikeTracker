13fe2f6ebd9468fae7b31b7137c4002bc5c52dfe
samuele giangreco s295413 <s295413@studenti.polito.it>
2023-01-04 13:52:22 +0100

    small be fix

--- backend/index.js
+++ backend/index.js
@@ -98,8 +98,8 @@ function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
   let R = 6371; // km
   let dLat = toRad(lat2 - lat1);
   let dLon = toRad(lon2 - lon1);
-  let lat1 = toRad(lat1);
-  let lat2 = toRad(lat2);
+  lat1 = toRad(lat1);
+  lat2 = toRad(lat2);
 
   let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
